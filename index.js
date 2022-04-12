6b00dabb046028e1d66b6c3ed65d22d031fb9694
sarthak19360 <97160163+sarthak19360@users.noreply.github.com>
2022-04-12 21:24:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img1").setAttribute("src",randomImageSource);
+document.querySelector(".img2").setAttribute("src",randomImageSource2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").textContent = "Player 1 Wins!";
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").textContent = "Player 2 Wins!";
+}else{
+    document.querySelector("h1").textContent = "Draw!";
+}
